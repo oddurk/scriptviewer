0d9d06cf901126c0f0239c947cfcf7938720f628
oddurk <oddur.kjartansson@bsc.es>
2014-11-06 16:36:19 +0100

    Adding Licence and Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+scriptviewer
+============
+A tool to visualize scripts. The tool recursively scans script files and lists other scripts which are called.
--- plotter/Plotter.py
+++ plotter/Plotter.py
@@ -5,7 +5,7 @@ import networkx as nx
 class Plotter(object):
     @staticmethod
     def plotGraph(graph):
-        pos = nx.spring_layout(graph, k=0.2) # positions for all nodes
+        pos = nx.spring_layout(graph, k=0.3) # positions for all nodes
         nx.draw_networkx_nodes(graph, pos, node_size=700, node_color='#ffffff')
         nx.draw_networkx_edges(graph, pos, edgelist=graph.edges(data=True), width=1, arrows=True)
         nx.draw_networkx_labels(graph, pos, font_size=12, font_family='sans-serif')
